83b5a311513e0eae538eeb928e1b7130a79e2cb9
danielgoettel <62118774+danielgoettel@users.noreply.github.com>
2026-03-01 21:10:52 -0500

    Config for first run
    Running 1 of each type of graph: default, geolayer, rf.

--- models/train_config.py
+++ models/train_config.py
@@ -2,7 +2,7 @@ def define_base_configuration():
     return {
         'synthetic_data': False,
         'percentage': 100,
-        'n_piezo_connected': 5,
+        'n_piezo_connected': 3,
         'W': 5,
         'gcn_true': True,
         'build_adj': False,  #ALWAYS FALSE
@@ -46,6 +46,7 @@ def define_base_configuration():
 
 # Define variations for each parameter (exclude 'in_dim' and 'out_dim')
 parameter_variations = {
+  'graph_type': ['geolayer', 'rf']
   #'feature_importance_multiplier' : [0.1],
   #'n_piezo_connected' : [4,6],
   #'n_pumps_connected' : [3],
