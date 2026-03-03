dee3508a02237a17fea6d8551000f00745dbaec7
danielgoettel <62118774+danielgoettel@users.noreply.github.com>
2026-03-03 03:00:15 -0500

    Remove TF32 matmul precision change

--- models/train_model.py
+++ models/train_model.py
@@ -615,7 +615,6 @@ def run_training_and_evaluation(config):
   
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
     print(f"Using {device} device.")
-    torch.set_float32_matmul_precision('medium')  # Use TF32 on A100 for faster matmul
 
     # Compute run output directory
     F_w = config.get('F_w', 3)
